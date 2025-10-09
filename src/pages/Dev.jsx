fdc64b9bb7437d306fd413d45f9c889322788c95
pwoofter <phoebewoofter@gmail.com>
2025-10-09 13:39:35 -0700

    Begins to style body of dev page

--- src/pages/Dev.jsx
+++ src/pages/Dev.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import impulse from '../assets/impulse.jpeg';
+import './Dev.css';
 
 export default function Dev() {
   return (
     <div>
       <h1>Development Projects</h1>
       <p>Explore my software engineering projects and GitHub repositories.</p>
+      <div>
+        <div className="impulse" style={{backgroundImage: `url(${impulse})`}}></div>
+      </div>
     </div>
   );
 }
